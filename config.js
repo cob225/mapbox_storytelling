09e8fe74d013d332a87f2726310be6b71e971f13
cob225 <obrie6cj@dukes.jmu.edu>
2025-05-09 17:49:38 -0400

    config.js

--- config.js
+++ config.js
@@ -67,7 +67,7 @@ var config = {
             hidden: false,
             title: 'Where Women Disappear from the Map',
             image: './assets/as_i_lay_dying.jpg',
-            description: 'The data for <em>The Color Purple</em> is particularly noticeable in its absences because no female "purchased from" data were reported. Celie\'s description of the stamps used to mail letters—"Little fat queen of England stamps"—shows a woman participating with a global communication and empire. However, the database only registers this as a male transaction, which could be a mistake in the data provided.',
+            description: 'The data for <em>The Color Purple</em> is particularly noticeable in its absences because no female "purchased from" data were reported. Celie\'s description of the stamps used to mail letters "Little fat queen of England stamps" shows a woman participating with a global communication and empire. However, the database only registers this as a male transaction, which could be a mistake in the data provided.',
             location: {
                 center: [-89.4, 33.5],
                 zoom: 6.2,
